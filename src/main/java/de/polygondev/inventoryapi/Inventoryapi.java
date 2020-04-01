c52d758e04e50adfc3fefe77aa7f7261849dd1f5
ChiAkuma <chiakuma13@gmail.com>
2020-04-02 01:03:13 +0200

    function onload added

--- src/main/java/de/polygondev/inventoryapi/Inventoryapi.java
+++ src/main/java/de/polygondev/inventoryapi/Inventoryapi.java
@@ -5,9 +5,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class Inventoryapi extends JavaPlugin {
 
     @Override
+    public void onLoad() {
+        // Plugin load logic
+    }
+
+    @Override
     public void onEnable() {
         // Plugin startup logic
-
     }
 
     @Override
