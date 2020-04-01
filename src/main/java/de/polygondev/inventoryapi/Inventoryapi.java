1798833431b065bdc6843c9deca8bd5f2b3622d9
ChiAkuma <chiakuma13@gmail.com>
2020-04-02 00:17:44 +0200

    Initial commit

---
+++ src/main/java/de/polygondev/inventoryapi/Inventoryapi.java
@@ -0,0 +1,17 @@
+package de.polygondev.inventoryapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Inventoryapi extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
